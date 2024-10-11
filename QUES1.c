5e9c32c9e1cc8f8372eda56f7811af7b93bf834e
CrAcker0010 <krishkumardixit8@gmail.com>
2024-10-11 21:10:38 +0530

    QUES1.c

--- QUES1.c
+++ QUES1.c
@@ -1,14 +1,33 @@
+void decToBinary(int n) {
+    int binaryNum[32]; // array to store binary number
+    int i = 0;
+    while (n > 0) {
+        binaryNum[i] = n % 2; // store remainder (0 or 1)
+        n = n / 2; // divide by 2
+        i++;
+    }
+    // print binary array in reverse order
+    for (int j = i - 1; j >= 0; j--) {
+        printf("%d", binaryNum[j]);
+    }
+}
+
 int main() {
-    int decimal_value;
+    int n;
     
     // Input from the user
     printf("Enter a decimal number: ");
-    scanf("%d", &decimal_value);
+    scanf("%d", &n);
     
        // Convert to octal
-    printf("Octal: %o\n", decimal_value);
+    printf("Octal: %o\n", n);
     
     // Convert to hexadecimal
-    printf("Hexadecimal: %X\n", decimal_value);
-   
+    printf("Hexadecimal: %X\n", n);
+    
+   // Conversion to binary
+   	printf("Binary: \n");
+   	decToBinary(n);
+   	
+   	
 }
