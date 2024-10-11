4971887c6727f9b6c510b8e432177c1f2aad971a
CrAcker0010 <krishkumardixit8@gmail.com>
2024-10-11 20:37:07 +0530

    Update QUES1.c

--- QUES1.c
+++ QUES1.c
@@ -1,19 +1,3 @@
-#include <stdio.h>
-
-void convertDecimal(int decimal_value) {
-    printf("Decimal: %d\n", decimal_value);
-    
-    // Convert to binary
-    printf("Binary: ");
-    for (int i = 31; i >= 0; i--) {
-        int bit = (decimal_value >> i) & 1;
-        printf("%d", bit);
-    }
-    printf("\n");
-    
- 
-}
-
 int main() {
     int decimal_value;
     
@@ -21,13 +5,10 @@ int main() {
     printf("Enter a decimal number: ");
     scanf("%d", &decimal_value);
     
-    convertDecimal(decimal_value);
-
        // Convert to octal
     printf("Octal: %o\n", decimal_value);
     
     // Convert to hexadecimal
     printf("Hexadecimal: %X\n", decimal_value);
-    
-   return 0; 
+   
 }
